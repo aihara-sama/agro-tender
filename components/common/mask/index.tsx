d79a6102f91036a1bb13d66bc21c6b6f46ff2b59
Aihara <aihara-sama@mail.ru>
2024-11-13 13:55:35 +0200

    feat: added common cmps & set up public paths

---
+++ components/common/mask/index.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+import { useEffect, useState, type FunctionComponent } from "react";
+
+interface Props {
+  onClick: () => void;
+}
+
+const Mask: FunctionComponent<Props> = ({ onClick }) => {
+  const [hasRendered, setHasRendered] = useState(false);
+
+  useEffect(() => {
+    setHasRendered(true);
+  }, []);
+
+  return (
+    <div
+      className={`fixed top-0 bottom-0 left-0 right-0 backdrop-filter z-[999]  transition-all ${clsx(hasRendered && "backdrop-blur-[2px] bg-mask visible")}`}
+      onClick={onClick}
+    ></div>
+  );
+};
+
+export default Mask;
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,23 @@
 import { updateSession } from "@/lib/supabase/middlewares/update-session";
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
+
+const publicPaths = [/\//, /\/orgs/, /\/sign-in/, /\/sign-up/];
 
 export async function middleware(request: NextRequest) {
+	const publicPathnameRegex = new RegExp(
+		`^(/)?(${publicPaths.map((page) => page.source).join("|")})$`,
+		"i"
+	);
+	const isPublicPath = publicPathnameRegex.test(request.nextUrl.pathname);
+
+	if (isPublicPath) return NextResponse.next();
+
 	return updateSession(request);
 }
 
 export const config = {
 	matcher: [
+		"/",
 		/*
 		 * Match all request paths except for the ones starting with:
 		 * - _next/static (static files)
@@ -14,6 +25,6 @@ export const config = {
 		 * - favicon.ico (favicon file)
 		 * Feel free to modify this pattern to include more paths.
 		 */
-		"/((?!sign-in|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+		"/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)|/$).*)",
 	],
 };
